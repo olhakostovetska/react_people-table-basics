6a0e9cc0fe3ed8dc6ca5feaf8c54eb6fad7726d2
YourGitHubUsername <YourGitHubEmail>
2025-03-15 16:08:48 +0200

    add task solution

---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { getPeople } from '../api';
+import { Loader } from '../components/Loader';
+import { PeopleTable } from '../components/PeopleTable';
+import { Person } from '../types/Person';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getPeople()
+      .then(setPeople)
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="title">People Page</h1>
+      {loading && <Loader />}
+      {error && (
+        <p data-cy="peopleLoadingError" className="has-text-danger">
+          Something went wrong
+        </p>
+      )}
+      {!loading && !error && <PeopleTable people={people} />}
+    </div>
+  );
+};
