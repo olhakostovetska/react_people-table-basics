5cd6c8a3403c09a0593b54b6a94715e74cf0dbbf
YourGitHubUsername <YourGitHubEmail>
2025-03-15 17:18:00 +0200

    add task solution

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -1,31 +1,59 @@
 import { useEffect, useState } from 'react';
-import { getPeople } from '../api';
 import { Loader } from '../components/Loader';
+import { Person } from '../types';
+import { getPeople } from '../api';
 import { PeopleTable } from '../components/PeopleTable';
-import { Person } from '../types/Person';
 
 export const PeoplePage = () => {
-  const [people, setPeople] = useState<Person[]>([]);
   const [loading, setLoading] = useState(true);
+  const [people, setPeople] = useState<Person[]>([]);
   const [error, setError] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
+    setPeople([]);
+
     getPeople()
-      .then(setPeople)
+      .then(data => {
+        const dataWithParent = data.map(person => {
+          const foundFather = data.find(
+            findingFather => findingFather.name === person.fatherName,
+          );
+          const foundMother = data.find(
+            findingMother => findingMother.name === person.motherName,
+          );
+
+          return { ...person, father: foundFather, mother: foundMother };
+        });
+
+        setPeople(dataWithParent);
+      })
       .catch(() => setError(true))
       .finally(() => setLoading(false));
   }, []);
 
   return (
-    <div>
+    <div className="container">
       <h1 className="title">People Page</h1>
-      {loading && <Loader />}
-      {error && (
-        <p data-cy="peopleLoadingError" className="has-text-danger">
-          Something went wrong
-        </p>
-      )}
-      {!loading && !error && <PeopleTable people={people} />}
+      <div className="block">
+        <div className="box table-container">
+          {loading && <Loader />}
+
+          {!loading && error && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!loading &&
+            !error &&
+            (people.length ? (
+              <PeopleTable people={people} />
+            ) : (
+              <p data-cy="noPeopleMessage">There are no people on the server</p>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
